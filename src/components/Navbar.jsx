cafdab15a9c783ee3aaa80e1bc4d6b7517e54057
Austin <austinofficial007@gmail.com>
2024-11-09 22:26:19 +0530

    router link udpate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <React.Fragment>
-            <nav className='navbar navbar-dark bg-dark navbar-expand-sm'>
+            <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
                 <div className="container">
-                    <Link to={'/'} className='navbar-brand'>
-                    <i class="fa-solid fa-user"></i> <span className='text-warning'>User Data</span>
+                    <Link to={"/"} className="navbar-brand">
+                        <i class="fa-solid fa-user"></i>{" "}
+                        <span className="text-warning">User Data</span>
                     </Link>
                 </div>
             </nav>
-
-            <h5 className='hompage-icon'><i class="fa-regular fa-hand-point-up"></i> Click above to begin</h5>
         </React.Fragment>
-    )
+    );
 }
